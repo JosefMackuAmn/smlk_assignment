f41f866fffd17c0e6477135cc420551a9e2c8b4e
JosefMackuAmn <josef.zsd@seznam.cz>
2021-10-27 11:57:19 +0200

    removed build

--- build/controllers/__test__/collController.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('POST /auth', () => {
-    it('sends 201 and creates new user', async () => {
-        expect(true).toBe(true);
-    });
-});
