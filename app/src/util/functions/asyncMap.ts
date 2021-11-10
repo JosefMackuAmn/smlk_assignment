4d2630b0f4b0d7b038350d0b36519d4336ace64c
JosefMackuAmn <josef.zsd@seznam.cz>
2021-11-10 09:18:34 +0100

    adjustment

--- app/src/util/functions/asyncMap.ts
+++ app/src/util/functions/asyncMap.ts
@@ -1,5 +1,5 @@
 // Calls async callback on every item in an input array
-// and waits for it to finish before returning an array
+// and waits for them to finish before returning an array
 // of callback's return values
 const asyncMap = async <P, T>(arr: T[], cb: (item: T, idx: number) => Promise<P>) => {
     return Promise.all(arr.map((item, idx) => {
